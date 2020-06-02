b5a91bd86e94622812e980350c113803aeba3ad0
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-06-03 02:03:48 +0300

    Adding TailwindCSS and fixing some visual bugs

--- frontend/src/components/TypingPage.tsx
+++ frontend/src/components/TypingPage.tsx
@@ -26,24 +26,32 @@ const TypingPage: React.FC = () => {
   }, []);
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    // making typescript happy
     if (!quote) return;
+
     if (startTime === 0) {
       setStartTime(Date.now());
     }
+
+    if (correct + wrong > quote.content.length) {
+      return;
+    }
+
     const char = String.fromCharCode(event.charCode);
 
     if (char === quote.content[correct] && wrong === 0) {
+      // checking the end condition here so it triggers appropriately
+      if (correct === quote.content.length - 1) {
+        setEndTime(Date.now());
+        setFinished(true);
+      }
       setCorrect(correct + 1)
     } else {
       setWrong(wrong + 1)
     }
-
-    if (correct === quote.content.length - 1) {
-      setEndTime(Date.now());
-      setFinished(true);
-    }
   }
 
+  // handling character removal
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key !== "Backspace") return;
 
@@ -58,27 +66,40 @@ const TypingPage: React.FC = () => {
     return <p>Loading...</p>;
   }
 
-  const typedCorrect = quote.content.substring(0, correct);
-  const typedWrong = quote.content.substring(correct, correct + wrong);
-  const notTyped = quote.content.substring(correct + wrong);
+  const totalTyped = correct + wrong;
+  let startIndex = quote.content.lastIndexOf(" ", totalTyped);
+  let endIndex = quote.content.indexOf(" ", totalTyped);
+  startIndex = startIndex === -1 ? totalTyped : startIndex;
+  endIndex = endIndex === -1 ? quote.content.length : endIndex;
+  
+  const correctBeforeEndIndex = correct > startIndex ? startIndex : correct;
+  const currentWordCorrectEndIndex = correct < startIndex ? startIndex : correct > endIndex ? endIndex : correct;
+  const currentWordWrongEndIndex = totalTyped > endIndex ? endIndex : totalTyped;
+  const typedWrongEndIndex = totalTyped > endIndex ? totalTyped : endIndex;
+
+  const typedCorrectBefore = quote.content.substring(0, correctBeforeEndIndex);
+  const typedWrongBefore = quote.content.substring(correctBeforeEndIndex, startIndex);
+  const currentWordCorrect = quote.content.substring(startIndex, currentWordCorrectEndIndex);
+  const currentWordWrong = quote.content.substring(currentWordCorrectEndIndex, currentWordWrongEndIndex);
+  const currentWordNotTyped = quote.content.substring(currentWordWrongEndIndex, endIndex);
+  const typedWrong = quote.content.substring(endIndex, typedWrongEndIndex);
+  const notTyped = quote.content.substring(typedWrongEndIndex);
 
   return (
-    <div>
-      <p>
-        <span style={{ color: 'green'}}>{typedCorrect}</span>
-        <span style={{ color: 'red', display: "inline-block"}}>{typedWrong}</span>
-        <span style={{
-          position: "absolute",
-          display: "inline-block",
-          width: "1px",
-          height: "16px",
-          fontWeight: "lighter",
-          backgroundColor: "black",
-          marginTop: "2px",
-          marginRight: "1px"
-        }}>&nbsp;</span>
-        <span>{notTyped}</span>
-      </p>
+    <div className="bg-blue-300 h-screen font-mono">
+      <div className="container mx-auto h-full flex justify-center items-center">
+        <div className="rounded bg-blue-100 w-2/3 shadow-lg px-6 py-6">
+        <p className="pb-4">
+          <span className="text-green-600">{typedCorrectBefore}</span>
+          <span className="text-red-600 bg-red-300">{typedWrongBefore}</span>
+          <span className="whitespace-no-wrap">
+          <span className="text-green-600">{currentWordCorrect}</span>
+          <span className="text-red-600 bg-red-300">{currentWordWrong}</span>
+          <span className="fake-caret">&nbsp;</span>
+          <span>{currentWordNotTyped}</span></span>
+          <span className="text-red-600 bg-red-300">{typedWrong}</span>
+          <span>{notTyped}</span>
+        </p>
       {finished ?
         <div>
           <p>Finished!</p>
@@ -86,11 +107,15 @@ const TypingPage: React.FC = () => {
         </div>
       :
         <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+                     leading-tight focus:outline-none focus:shadow-outline"
           onKeyPress={event => handleKeyPress(event)}
           onKeyDown={event => handleKeyDown(event)}
           maxLength={quote.content.length}
         />
       }
+      </div>
+    </div>
     </div>
   );
 };
